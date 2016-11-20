18cc6e8f934d9209f29d0800f679d7c5607cc519
GrenRT <grenintegra@gmail.com>
2016-11-20 16:47:38 +0400

    Добавлен класс Point и функция вычисления расстояния между точками

--- sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
@@ -13,12 +13,25 @@ public class MyFirstProgram {
       Rectangle r = new Rectangle(4, 6);
       System.out.println("Площадь прямоугольника со сторонами " + r.a + " и " + r.b + " = " + r.area());
 
+      Point p1 = new Point();
+      Point p2 = new Point();
+      p1.x = 1;
+      p2.x = 10;
+      p1.y = 0;
+      p2.y = 0;
+      System.out.println("Расстояние между точками A(" + p1.x + ":" + p1.y + ") и В(" + p2.x + ":" + p2.y + ") = " + distance(p1, p2));
     }
 
     public static void hello (String somebody) {
         System.out.println("Hello, " + somebody + "!");
     }
 
+  public static double distance(Point p1, Point p2){
+       return Math.sqrt((p2.x - p1.x) * (p2.x - p1.x) + (p2.y - p1.y) * (p2.y - p1.y));
+
+
+
+  }
 
 
 }
---
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/Point.java
@@ -0,0 +1,10 @@
+package ru.stqa.pft.sandbox;
+
+/**
+ * Created by gren on 20.11.2016.
+ */
+public class Point {
+
+  public double x;
+  public double y;
+}
