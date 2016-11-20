818e4ffb613b76cfe0145382d984081aa4e5747c
GrenRT <grenintegra@gmail.com>
2016-11-20 16:53:14 +0400

    Функция Point заменена на метод

--- sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
@@ -13,12 +13,8 @@ public class MyFirstProgram {
       Rectangle r = new Rectangle(4, 6);
       System.out.println("Площадь прямоугольника со сторонами " + r.a + " и " + r.b + " = " + r.area());
 
-      Point p1 = new Point();
-      Point p2 = new Point();
-      p1.x = 1;
-      p2.x = 10;
-      p1.y = 0;
-      p2.y = 0;
+      Point p1 = new Point(1, 0);
+      Point p2 = new Point(10, 0);
       System.out.println("Расстояние между точками A(" + p1.x + ":" + p1.y + ") и В(" + p2.x + ":" + p2.y + ") = " + distance(p1, p2));
     }
 
