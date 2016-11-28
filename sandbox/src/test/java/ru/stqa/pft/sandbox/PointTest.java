7b0ea69eae14fe18e1ed881f37fd63aaf99fb26d
GrenRT <grenintegra@gmail.com>
2016-11-28 12:41:22 +0400

    Добавлены тесты для класса Point

---
+++ sandbox/src/test/java/ru/stqa/pft/sandbox/PointTest.java
@@ -0,0 +1,27 @@
+package ru.stqa.pft.sandbox;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+/**
+ * Created by razgonyaev on 28.11.2016.
+ */
+public class PointTest {
+
+  @Test
+  public void testDistance() {
+    Point p1 = new Point(0, 0);
+    Point p2 = new Point(10, 0);
+    Assert.assertEquals(p1.distance(p2), 10.0);
+ }
+ @Test
+  public void testDistance2() {
+    Point p1 = new Point(0, 0);
+    Assert.assertEquals(p1.distance(p2), 10.0);
+  }
+  @Test
+  public void testDistance3() {
+    Point p1 = new Point(0, 0);
+    Assert.assertEquals(p1.distance(p2), Math.sqrt(109));
+  }
+}
--- sandbox/src/test/java/ru/stqa/pft/sandbox/SquareTests.java
+++ sandbox/src/test/java/ru/stqa/pft/sandbox/SquareTests.java
@@ -1,8 +1,7 @@
-package ru.stqa.ptf.sandbox;
+package ru.stqa.pft.sandbox;
 
 import org.testng.Assert;
 import org.testng.annotations.Test;
-import ru.stqa.pft.sandbox.Square;
 
 /**
  * Created by gren on 28.11.2016.
@@ -10,7 +9,7 @@ import ru.stqa.pft.sandbox.Square;
 public class SquareTests {
 
   @Test
-    public void testArea() {
+  public void testArea() {
     Square s = new Square(5);
     Assert.assertEquals(s.area(), 25.0);
 
