c6d232a94f6d2bb7481a907703d5acf050658b4b
GrenRT <grenintegra@gmail.com>
2016-11-29 11:14:04 +0400

    Исправлен тест для класса Point

--- sandbox/src/test/java/ru/stqa/pft/sandbox/PointTest.java
+++ sandbox/src/test/java/ru/stqa/pft/sandbox/PointTest.java
@@ -4,7 +4,7 @@ import org.testng.Assert;
 import org.testng.annotations.Test;
 
 /**
- * Created by razgonyaev on 28.11.2016.
+ * Created by gren on 28.11.2016.
  */
 public class PointTest {
 
@@ -17,11 +17,13 @@ public class PointTest {
  @Test
   public void testDistance2() {
     Point p1 = new Point(0, 0);
+    Point p2 = new Point(0, -10);
     Assert.assertEquals(p1.distance(p2), 10.0);
   }
   @Test
   public void testDistance3() {
     Point p1 = new Point(0, 0);
+    Point p2 = new Point(-3, 10);
     Assert.assertEquals(p1.distance(p2), Math.sqrt(109));
   }
 }
