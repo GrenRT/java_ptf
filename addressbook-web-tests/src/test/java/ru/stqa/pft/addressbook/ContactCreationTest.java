766461346d320185a15941508c25eacc45fc4fa1
GrenRT <grenintegra@gmail.com>
2016-12-01 11:55:28 +0400

    Создан тест создания контакта

---
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/ContactCreationTest.java
@@ -0,0 +1,118 @@
+package ru.stqa.pft.addressbook;
+
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.Test;
+
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.*;
+
+import java.util.Random;
+
+public class ContactCreationTest {
+  FirefoxDriver wd;
+  Random r = new Random();
+  int add = r.nextInt(10);
+
+  @BeforeMethod
+  public void setUp() throws Exception {
+    wd = new FirefoxDriver();
+    wd.manage().timeouts().implicitlyWait(60, TimeUnit.SECONDS);
+    wd.get("http://localhost/addressbook/");
+    login();
+  }
+
+  private void login() {
+    wd.findElement(By.name("user")).click();
+    wd.findElement(By.name("user")).clear();
+    wd.findElement(By.name("user")).sendKeys("admin");
+    wd.findElement(By.name("pass")).click();
+    wd.findElement(By.name("pass")).clear();
+    wd.findElement(By.name("pass")).sendKeys("secret");
+    wd.findElement(By.xpath("//form[@id='LoginForm']/input[3]")).click();
+  }
+
+  @Test
+  public void testContactCreation() {
+    gotoContactCreationForm();
+        fillContactForm(getParamsObject());
+    submitContactCreation();
+  }
+
+  protected ParamsObject getParamsObject() {
+    ParamsObject paramsObject = new ParamsObject();
+    paramsObject.setNickName("Test" + add);
+    paramsObject.setFirstName("Test" + add);
+    paramsObject.setMiddleName("Test" + add);
+    paramsObject.setLastName("Test" + add);
+    paramsObject.setTitle("title");
+    paramsObject.setCompanyName("company");
+    paramsObject.setAddress("address");
+    paramsObject.setHomePhone("" + (r.nextInt(100000) + 1000000));
+    paramsObject.setMobilePhone("+7987" + r.nextInt(1000000));
+    paramsObject.setEmail1("test@test.test");
+    paramsObject.setEmail2("test2@test.test");
+    return paramsObject;
+  }
+
+
+  private void submitContactCreation() {
+    wd.findElement(By.xpath("//div[@id='content']/form/input[21]")).click();
+  }
+
+  private void fillContactForm(ParamsObject paramsObject) {
+    wd.findElement(By.name("firstname")).click();
+    wd.findElement(By.name("firstname")).clear();
+    wd.findElement(By.name("firstname")).sendKeys(paramsObject.getFirstName());
+    wd.findElement(By.name("middlename")).click();
+    wd.findElement(By.name("middlename")).clear();
+    wd.findElement(By.name("middlename")).sendKeys(paramsObject.getMiddleName());
+    wd.findElement(By.name("lastname")).click();
+    wd.findElement(By.name("lastname")).clear();
+    wd.findElement(By.name("lastname")).sendKeys(paramsObject.getLastName());
+    wd.findElement(By.name("nickname")).click();
+    wd.findElement(By.name("nickname")).clear();
+    wd.findElement(By.name("nickname")).sendKeys(paramsObject.getNickName());
+    wd.findElement(By.name("title")).click();
+    wd.findElement(By.name("title")).clear();
+    wd.findElement(By.name("title")).sendKeys(paramsObject.getTitle());
+    wd.findElement(By.name("company")).click();
+    wd.findElement(By.name("company")).clear();
+    wd.findElement(By.name("company")).sendKeys(paramsObject.getCompanyName());
+    wd.findElement(By.name("address")).click();
+    wd.findElement(By.name("address")).clear();
+    wd.findElement(By.name("address")).sendKeys(paramsObject.getAddress());
+    wd.findElement(By.name("home")).click();
+    wd.findElement(By.name("home")).clear();
+    wd.findElement(By.name("home")).sendKeys(paramsObject.getHomePhone());
+    wd.findElement(By.name("mobile")).click();
+    wd.findElement(By.name("mobile")).clear();
+    wd.findElement(By.name("mobile")).sendKeys(paramsObject.getMobilePhone());
+    wd.findElement(By.name("email")).click();
+    wd.findElement(By.name("email")).clear();
+    wd.findElement(By.name("email")).sendKeys(paramsObject.getEmail1());
+    wd.findElement(By.name("email2")).click();
+    wd.findElement(By.name("email2")).clear();
+    wd.findElement(By.name("email2")).sendKeys(paramsObject.getEmail2());
+  }
+
+  private void gotoContactCreationForm() {
+    wd.findElement(By.linkText("add new")).click();
+  }
+
+  @AfterMethod
+  public void tearDown() {
+    wd.quit();
+  }
+
+  public static boolean isAlertPresent(FirefoxDriver wd) {
+    try {
+      wd.switchTo().alert();
+      return true;
+    } catch (NoAlertPresentException e) {
+      return false;
+    }
+  }
+}
---
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/ParamsObject.java
@@ -0,0 +1,114 @@
+package ru.stqa.pft.addressbook;
+
+/**
+ * Created by razgonyaev on 01.12.2016.
+ */
+public class ParamsObject {
+
+  private String firstName;
+  private String middleName;
+  private String lastName;
+  private String nickName;
+  private String title;
+  private String companyName;
+  private String address;
+  private String homePhone;
+  private String mobilePhone;
+  private String email1;
+  private String email2;
+
+  public String getEmail2() {
+    return email2;
+  }
+
+  public void setEmail2(String email2) {
+    this.email2 = email2;
+  }
+
+  public String getEmail1() {
+
+    return email1;
+  }
+
+  public void setEmail1(String email1) {
+    this.email1 = email1;
+  }
+
+  public String getMobilePhone() {
+
+    return mobilePhone;
+  }
+
+  public void setMobilePhone(String mobilePhone) {
+    this.mobilePhone = mobilePhone;
+  }
+
+  public String getAddress() {
+
+    return address;
+  }
+
+  public void setAddress(String address) {
+    this.address = address;
+  }
+
+  public String getHomePhone() {
+
+    return homePhone;
+  }
+
+  public void setHomePhone(String homePhone) {
+    this.homePhone = homePhone;
+  }
+
+  public String getCompanyName() {
+
+    return companyName;
+  }
+
+  public void setCompanyName(String companyName) {
+    this.companyName = companyName;
+  }
+
+  public String getTitle() {
+
+    return title;
+  }
+
+  public void setTitle(String title) {
+    this.title = title;
+  }
+
+  public String getLastName() {
+
+    return lastName;
+  }
+
+  public void setLastName(String lastName) {
+    this.lastName = lastName;
+  }
+
+  public String getNickName() {
+    return nickName;
+  }
+
+  public void setNickName(String nickName) {
+    this.nickName = nickName;
+  }
+
+  public String getFirstName() {
+    return firstName;
+  }
+
+  public void setFirstName(String firstName) {
+    this.firstName = firstName;
+  }
+
+  public String getMiddleName() {
+    return middleName;
+  }
+
+  public void setMiddleName(String middleName) {
+    this.middleName = middleName;
+  }
+}
