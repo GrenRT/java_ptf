7c5ca3a28e6d36a6332611b95225b5c278ae6847
GrenRT <grenintegra@gmail.com>
2016-12-04 17:26:35 +0400

    Добавлена проверка вырожденных уравнений и тесты для них

--- sandbox/src/test/java/ru/stqa/pft/sandbox/EqutionTest.java
+++ sandbox/src/test/java/ru/stqa/pft/sandbox/EqutionTest.java
@@ -25,4 +25,22 @@ public class EqutionTest {
     Eqution e = new Eqution(1,5,6);
     Assert.assertEquals(e.rootNumber(), 2);
   }
+
+  @Test
+  public void testLinear() {
+    Eqution e = new Eqution(0,1,1);
+    Assert.assertEquals(e.rootNumber(), 1);
+  }
+
+  @Test
+  public void testConstant() {
+    Eqution e = new Eqution(0,0,1);
+    Assert.assertEquals(e.rootNumber(), 0);
+  }
+
+  @Test
+  public void testZero() {
+    Eqution e = new Eqution(0,0,0);
+    Assert.assertEquals(e.rootNumber(), -1);
+  }
 }
