87e46049465dfd2f148e628a64d6defe2b92265b
GrenRT <grenintegra@gmail.com>
2016-12-17 18:02:21 +0400

    Добавлена проверка списков в тест удаления контакта

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactDeletionTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactDeletionTests.java
@@ -1,7 +1,10 @@
 package ru.stqa.pft.addressbook.tests;
 
+import org.testng.Assert;
 import org.testng.annotations.Test;
 import ru.stqa.pft.addressbook.model.ContactData;
+
+import java.util.List;
 
 /**
  * Created by gren on 04.12.2016.
@@ -14,7 +17,15 @@ public class ContactDeletionTests extends TestBase {
     if (!  app.getContactHelper().isThereAContact()) {
       app.getContactHelper().createContact(new ContactData(0,"Test1", "Test1"));
     }
-        app.getContactHelper().openModificationForm();
+    List<ContactData> before = app.getContactHelper().getContactList();
+    app.getContactHelper().selectContact(before.size() - 1);
     app.getContactHelper().deleteContact();
+    app.getContactHelper().closeWindows();
+    app.getNavigationHelper().gotoHome();
+    List<ContactData> after = app.getContactHelper().getContactList();
+    Assert.assertEquals(after.size(), before.size() - 1);
+
+    before.remove(before.size() - 1);
+    Assert.assertEquals(before, after);
   }
 }
