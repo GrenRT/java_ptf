00c8ccc0368e39ef15a5c57018fd3086184a56d7
GrenRT <grenintegra@gmail.com>
2016-12-27 10:34:56 +0400

    Генератор данных переделан для сохранения в xml, в тесте создания контакта данные грузятся из xml

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupCreationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupCreationTests.java
@@ -1,14 +1,18 @@
 package ru.stqa.pft.addressbook.tests;
 
+import com.thoughtworks.xstream.XStream;
 import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 import ru.stqa.pft.addressbook.model.GroupData;
 import ru.stqa.pft.addressbook.model.Groups;
 
-import java.io.*;
-import java.util.ArrayList;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
 import java.util.Iterator;
 import java.util.List;
+import java.util.stream.Collectors;
 
 import static org.hamcrest.CoreMatchers.equalTo;
 import static org.hamcrest.MatcherAssert.assertThat;
@@ -17,15 +21,17 @@ public class GroupCreationTests extends TestBase {
 
   @DataProvider
   public Iterator<Object[]> validGroup() throws IOException {
-    List<Object[]> list = new ArrayList<Object[]>();             //Заполняем список массивох тестовых данных
-    BufferedReader reader = new BufferedReader( new FileReader(new File("src/test/resources/groups.csv"))); //Создаем объект, из которого можно читать строки
+    BufferedReader reader = new BufferedReader( new FileReader(new File("src/test/resources/groups.xml"))); //Создаем объект, из которого можно читать строки
+    String xml = "";
     String line = reader.readLine(); //читаем первую строку
     while (line != null) {  //цикл выполняется пока есть строки
-      String[] split = line.split(";"); //строки режутся на тестовые данные
-      list.add(new Object[] {new GroupData().withName(split[0]).withHeader(split[1]).withFooter(split[2])}); //заполнение тестовыми данными объекта GroupData
+      xml += line;
       line = reader.readLine(); //читается новая строка
     }
-    return list.iterator();
+    XStream xstream = new XStream();
+    xstream.processAnnotations(GroupData.class);
+    List<GroupData> groups = (List<GroupData>) xstream.fromXML(xml);
+    return groups.stream().map((g) -> new Object[] {g}).collect(Collectors.toList()).iterator();
   }
 
   @Test(dataProvider = "validGroup")
