2ca4265e8a0183e94774a06af3717795183c0ba0
GrenRT <grenintegra@gmail.com>
2017-01-12 12:34:27 +0400

    Добавлено логирование в тест создания групп(6-11)

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/TestBase.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/TestBase.java
@@ -1,14 +1,24 @@
 package ru.stqa.pft.addressbook.tests;
 
 import org.openqa.selenium.remote.BrowserType;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.testng.annotations.AfterMethod;
 import org.testng.annotations.AfterSuite;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.BeforeSuite;
 import ru.stqa.pft.addressbook.appmanager.ApplicationManager;
+
+import java.lang.reflect.Method;
+import java.util.Arrays;
 
 /**
  * Created by razgonyaev on 01.12.2016.
  */
 public class TestBase {
+
+  Logger logger = LoggerFactory.getLogger(TestBase.class);
+
 
   protected static final ApplicationManager app
           = new ApplicationManager(System.getProperty("browser", BrowserType.CHROME));  //Через системную проперть передает тип браузера, если нет, то используется Хром
@@ -18,9 +28,18 @@ public class TestBase {
     app.init();
   }
 
-  @AfterSuite
+  @AfterSuite(alwaysRun = true)
   public void tearDown() {
     app.stop();
   }
 
+  @BeforeMethod
+  public void logTestStart(Method m, Object[] p) {
+    logger.info("Start test " + m.getName() + "with parameters" + Arrays.asList(p));
+  }
+
+  @AfterMethod(alwaysRun = true)
+  public void logTestStop (Method m, Object[] p) {
+    logger.info("Stop test " + m.getName());
+  }
 }
