6db176327bdd5b82c64b47ef98953a7f98922c65
GrenRT <grenintegra@gmail.com>
2017-01-30 18:37:11 +0400

    Отключение капчи для выполнения теста(8-5)

--- mantis-tests/src/test/java/ru/stqa/pft/mantis/tests/TestBase.java
+++ mantis-tests/src/test/java/ru/stqa/pft/mantis/tests/TestBase.java
@@ -5,6 +5,9 @@ import org.testng.annotations.AfterSuite;
 import org.testng.annotations.BeforeSuite;
 import ru.stqa.pft.mantis.appmanager.ApplicationManager;
 
+import java.io.File;
+import java.io.IOException;
+
 /**
  * Created by razgonyaev on 01.12.2016.
  */
@@ -16,10 +19,12 @@ public class TestBase {
   @BeforeSuite
   public void setUp() throws Exception {
     app.init();
+    app.ftp().upload(new File("src/test/resources/config_inc.php"), "config_inc.php", "config_inc.php.backup");
   }
 
   @AfterSuite(alwaysRun = true)
-  public void tearDown() {
+  public void tearDown() throws IOException {
+    app.ftp().restore("config_inc.php.backup", "config_inc.php");
     app.stop();
   }
 
